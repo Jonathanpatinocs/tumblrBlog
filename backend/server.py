6f1cf3308491407a1bc04820a8209758b86ec430
Jonathanpatinocs <jonathanpatinocs@gmail.com>
2024-08-15 22:01:08 -0400

    pytumblr installed

--- backend/server.py
+++ backend/server.py
@@ -1,5 +1,9 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
+import pytumblr
+from creds import CONSUMER_KEY, CONSUMER_SECRET, TOKEN, TOKEN_SECRET, ACCESS_TOKEN
+
+
 
 
 app = Flask(__name__)
@@ -7,6 +11,25 @@ cors = CORS(app, origins='*')
 
 
 @app.route("/posts", methods=['GET'])
+
+# Authenticate via OAuth
+def clientfunc():
+
+    client = pytumblr.TumblrRestClient(
+        CONSUMER_KEY,
+        CONSUMER_SECRET,
+        TOKEN,
+        TOKEN_SECRET
+    )
+    
+    posts = client.posts('johnyyy0-0.tumblr.com')
+    print(posts)
+
+
+
+clientfunc()
+# Make the request
+
 
 def posts(): 
     return jsonify(
